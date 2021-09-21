104fedc4e61abf2244dbbff059aa3b68753ad5fe
ant1k9 <ant1k9@protonmail.com>
2021-09-21 04:27:54 +0300

    Update show stats helper to use with single coin

--- show_stats.fish
+++ show_stats.fish
@@ -1,9 +1,19 @@
 #!/usr/bin/env fish
 
-for rate in (psql $DB_DSN -qXAt -c 'select name from coins order by rank')
-    echo -e Rate: $rate "\n"
+function show_graph
     psql $DB_DSN -qXAt \
-        -c "select value from rates where coin_uuid = (select uuid from coins where name = '$rate') order by ts" \
+        -c "select value from rates where coin_uuid = (select uuid from coins where name = '$argv[1]') order by ts" \
         | asciigraph -w 200 -h 16
-    echo; echo; sleep 2
+    echo -e "\n"
+end
+
+if test (count $argv) -eq 1
+    show_graph "$argv[1]"
+    exit
+end
+
+for rate in (psql $DB_DSN -qXAt -c 'select name from coins order by rank')
+    echo -e "Rate: $rate \n"
+    show_graph "$rate"
+    sleep 2
 end
